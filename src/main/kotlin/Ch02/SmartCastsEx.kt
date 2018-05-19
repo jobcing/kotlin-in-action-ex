1b06a06d25967c62f9dae9363c99927c8c4aede0
Cho ByeongChan <jobc0335@gmail.com>
2018-05-19 17:00:26 +0900

    add smart casts example

---
+++ src/main/kotlin/Ch02/SmartCastsEx.kt
@@ -0,0 +1,26 @@
+package Ch02
+
+/**
+ * Created by ByeongChan on 2018. 5. 19..
+ */
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int {
+    if(e is Num) {
+        val n = e as Num
+        return n.value
+    }
+
+    if(e is Sum) {
+        return eval(e.right) + eval(e.left)
+    }
+
+    throw IllegalArgumentException("Unknown expression")
+}
+
+fun main(args: Array<String>) {
+    println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+}
