10e8ea67dbf2c125a4c974bf8c276dcaa425af4d
Cho ByeongChan <jobc0335@gmail.com>
2018-05-19 16:18:08 +0900

    add properteis example

--- src/main/kotlin/Ch02/Properteis01.kt
+++ src/main/kotlin/Ch02/Properteis01.kt
@@ -3,3 +3,14 @@ package Ch02
 /**
  * Created by ByeongChan on 2018. 5. 19..
  */
+
+class Person(
+        val name: String,
+        val isMarried: Boolean
+)
+
+fun main(args: Array<String>) {
+    val person = Person("Bob", true)
+    println(person.name)
+    println(person.isMarried)
+}
