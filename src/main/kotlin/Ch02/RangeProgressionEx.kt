990345bacc1af7b65619af1120c1a31fd9513390
Cho ByeongChan <jobc0335@gmail.com>
2018-05-20 15:55:57 +0900

    add range progression example

---
+++ src/main/kotlin/Ch02/RangeProgressionEx.kt
@@ -0,0 +1,18 @@
+package Ch02
+
+/**
+ * Created by ByeongChan on 2018. 5. 20..
+ */
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz "
+    i % 3 == 0 -> "Fizz "
+    i % 5 == 0 -> "Buzz "
+    else -> "$i "
+}
+
+fun main(args: Array<String>) {
+    for (i in 100 downTo 1 step 2) {
+        print(fizzBuzz(i))
+    }
+}
