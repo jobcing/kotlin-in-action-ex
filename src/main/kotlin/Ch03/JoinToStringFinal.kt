2bca894e278a2089fef6a2926a9f76ff759e4de8
Cho ByeongChan <jobc0335@gmail.com>
2018-06-02 16:02:24 +0900

    add join to string final example

---
+++ src/main/kotlin/Ch03/JoinToStringFinal.kt
@@ -0,0 +1,27 @@
+package Ch03
+
+/**
+ * Created by ByeongChan on 2018. 6. 2..
+ */
+
+fun <T> Collection<T>.joinToString(
+        separator: String = ", ",
+        prefix: String = "",
+        postfix: String = ""
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(list.joinToString(separator = "; ",
+            prefix = "(", postfix = ")"))
+}
